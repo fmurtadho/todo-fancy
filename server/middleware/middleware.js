a29cf6aae9d1293cf9d0f9d42e0bb3c5c6b47be4
fmurtadho <fmurtadho@gmail.com>
2018-10-10 18:14:05 +0700

    repair middleware

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,6 +1,7 @@
 const User = require('../models/users')
 const jwt = require('jsonwebtoken')
 const Todo = require('../models/todo')
+const Mongoose = require('mongoose');
 
 class Middleware {
     static authenticate(req, res, next) {
@@ -11,6 +12,7 @@ class Middleware {
                     User.findById(decoded.userId)
                         .then(function (user) {
                             req.id = user._id
+                            req.dataUser = user
                             next()
                         })
                 } else {
@@ -27,38 +29,50 @@ class Middleware {
         }
     }
 
-    static isHim(req, res, next) {
-        let token = req.headers.token
-        let check = false
-        // console.log('ini req params id nyaaaa',req.params.id)
-        if (token) {
-            jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
-                if (!err) {
-                    User.findById(decoded.userId)
-                        .then(function (user) {
-                            for(let i = 0 ; i < user.todoList.length ; i ++){
-                                if(req.params.id === user.todoList[i]){
-                                    check = true
-                                }
-                            }
+    // static isHim(req, res, next) {
+    //     let token = req.headers.token
+    //     let check = false
+    //     // console.log('ini req params id nyaaaa',req.params.id)
+    //     if (token) {
+    //         jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+    //             if (!err) {
+    //                 User.findById(decoded.userId)
+    //                     .then(function (user) {
+    //                         for(let i = 0 ; i < user.todoList.length ; i ++){
+    //                             if(req.params.id === user.todoList[i]){
+    //                                 check = true
+    //                             }
+    //                         }
 
-                            if(check = true){
-                                next()
-                            }
-                        })
-                } else {
-                    res.status(403).json({
-                        message: 'invalid token'
-                    })
-                }
-            })
-        } else {
-            console.log('token not found')
-            res.status(403).json({
-                message: 'token not found'
-            })
-        }
+    //                         if(check = true){
+    //                             req.user = decoded
+    //                             next()
+    //                         }
+    //                     })
+    //             } else {
+    //                 res.status(403).json({
+    //                     message: 'invalid token'
+    //                 })
+    //             }
+    //         })
+    //     } else {
+    //         console.log('token not found')
+    //         res.status(403).json({
+    //             message: 'token not found'
+    //         })
+    //     }
         
+    // }
+
+    static isHim(req,res,next){
+        const id = new Mongoose.Types.ObjectId(req.params.id)
+
+        User.find({'todoList' : {$in:id}})
+        .then(data =>{
+            if(data[0].id === req.id.toString()){
+                next()
+            }
+        })
     }
 }
 
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -4,9 +4,9 @@ const middleware = require('../middleware/middleware')
 
 router.post('/create',middleware.authenticate, todoController.create)
 router.get('/read',middleware.authenticate,todoController.read)
-router.put('/update/:id',middleware.isHim,todoController.update)
-router.delete('/delete/:id',middleware.isHim,todoController.delete)
-router.put('/complete/:id',middleware.isHim,todoController.complete)
-router.put('/uncomplete/:id',middleware.isHim,todoController.uncomplete)
+router.put('/update/:id',middleware.authenticate,middleware.isHim,todoController.update)
+router.delete('/delete/:id',middleware.authenticate,middleware.isHim,todoController.delete)
+router.put('/complete/:id',middleware.authenticate,middleware.isHim,todoController.complete)
+router.put('/uncomplete/:id',middleware.authenticate,middleware.isHim,todoController.uncomplete)
 
 module.exports = router
